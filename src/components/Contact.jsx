40600f47c806dd2a7b43db240e49e60162106ef8
burhan <burhanweb111@gmail.com>
2025-07-09 17:41:42 +0530

    updated Contact details

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,11 +3,10 @@ function Contact() {
     <div className="pb-24 text-center" id="contact">
       <h2 className="my-20 text-4xl font-semibold">Contact Me</h2>
       <p className="text-neutral-300 mb-4 text-lg">
-        Feel free to reach out via email or phone.
+        Feel free to reach out via email
       </p>
       <div className="space-y-2 text-xl text-neutral-200">
         <p>
-          📧{" "}
           <a
             href="mailto:burhanweb11@gmail.com"
             className="text-cyan-400 hover:underline"
@@ -15,7 +14,6 @@ function Contact() {
             burhanweb111@gmail.com
           </a>
         </p>
-        <p>📞 +91 95964 02602</p>
       </div>
     </div>
   );
